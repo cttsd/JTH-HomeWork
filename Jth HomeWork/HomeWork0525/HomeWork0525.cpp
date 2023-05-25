4e8168b904eb05ae8050caf459473eb8aa43317e
태훈 정 <c0t0h0@naver.com>
2023-05-25 19:48:51 +0900

    homework

---
+++ Jth HomeWork/HomeWork0525/HomeWork0525.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <conio.h>
+#include <iostream>
+#include "player.h"
+#include "Monster.h"
+#include "FightZone.h"
+
+
+
+
+// 문법은 왜 발전했다? 인간이 코드를 덜치기 위해서 발전했습니다.
+int main()
+{
+	// 객체지향이라고 한다.
+	Player NewPlayer;
+	Monster NewMonster;
+
+	FightZone FZone;
+	FZone.Fight(NewPlayer, NewMonster);
+
+}
+
+
